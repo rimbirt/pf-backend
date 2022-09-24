ef3884e8e05f51e97397df754f08ac07bae0d239
Ramiro Iglesias <ramiro.iglesias@birt.eus>
2022-09-24 11:52:12 +0200

    Editor config + failing test

--- src/test/java/eus/birt/pfbackend/PfBackendApplicationTests.java
+++ src/test/java/eus/birt/pfbackend/PfBackendApplicationTests.java
@@ -3,9 +3,11 @@ package eus.birt.pfbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest class PfBackendApplicationTests {
+class PfBackendApplicationTests {
 
-    @Test void contextLoads() {
-    }
+  @Test
+  void contextLoads() throws Exception {
+    throw new Exception();
+  }
 
 }
